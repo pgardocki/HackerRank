9defdc7c1c9e0feced78f6e70b8b5cc273dc72d7
Patrick <p.gardocki@gmail.com>
2016-01-21 21:13:10 -0500

    Add days 20 and 21

--- 30 Days of Code (Java)/Days 20-24.java
+++ 30 Days of Code (Java)/Days 20-24.java
@@ -1,6 +1,38 @@
 //////////////////////// Day 20: Review + More String Methods!
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String toSolve = sc.nextLine();
+      String delimiter = "[!,?.\\_'@ ]+";
+      
+      StringTokenizer st = new StringTokenizer(toSolve, delimiter);
+      int totalWords = st.countTokens();
+      
+      System.out.println(totalWords);
+      
+      while (st.hasMoreTokens()) {
+        System.out.println(st.nextToken());
+      }
+    }
+}
+
 //////////////////////// Day 21: Generics!
+
+class Printer {
+  public <E> void printArray(E[] array) {
+    for (E element : array) {
+      System.out.println(element);
+    }
+  } 
+}
 
 //////////////////////// Day 22: Heaps and Binary Trees!
 
