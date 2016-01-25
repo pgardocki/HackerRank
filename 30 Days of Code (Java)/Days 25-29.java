b6410c22a398d4bf7fc948ce281889d4bf2e0bb1
Patrick <p.gardocki@gmail.com>
2016-01-25 10:32:16 -0500

    Add day 25

--- 30 Days of Code (Java)/Days 25-29.java
+++ 30 Days of Code (Java)/Days 25-29.java
@@ -1,9 +1,39 @@
 //////////////////////// Day 25: Running Time and Complexity!
 
-//////////////////////// Day 26: Front End + Implementations!
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-//////////////////////// Day 27: Testing!
+public class Solution {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    for (int i = in.nextInt(); i > 0; i--) {
+      int toCheck = in.nextInt();
+      String prime = isPrime(toCheck) ? "Prime" : "Not prime";
+      System.out.println(prime);
+    }
+  }
+  
+  static boolean isPrime(int num) {
+    if (num == 2) return true;
+    if (num <= 1 || num % 2 == 0) return false;    
+    
+    double i = Math.sqrt(num);
+    for (int j = 3; j <= i; j += 2) {
+      if (num % j == 0) return false;
+    }
+    return true;
+  }
+}
 
-//////////////////////// Day 28: Back End + How to Use/Read Documentation!
+//////////////////////// Day 26: Testing Part I + Implementations!
+
+
+
+//////////////////////// Day 27: Testing Part II!
+
+//////////////////////// Day 28: Introduction to Databases & More Review!
 
 //////////////////////// Day 29: Look at Everything We've Learned!
