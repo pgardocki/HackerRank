6e349722225e1f7071e7809723c5f53d06a12f49
Patrick <p.gardocki@gmail.com>
2016-01-26 10:55:39 -0500

    Add day 26

--- 30 Days of Code (Java)/Days 25-29.java
+++ 30 Days of Code (Java)/Days 25-29.java
@@ -30,7 +30,38 @@ public class Solution {
 
 //////////////////////// Day 26: Testing Part I + Implementations!
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+  public static void main(String[] args) {
+    // Calculate Library Late Fees
+    Scanner in = new Scanner(System.in);
+    
+    int userReturnDay = in.nextInt();
+    int userReturnMonth = in.nextInt();
+    int userReturnYear = in.nextInt();
+    
+    int libraryDueDay = in.nextInt();
+    int libraryDueMonth = in.nextInt();
+    int libraryDueYear = in.nextInt();
+    
+    if (libraryDueYear - userReturnYear < 0) {
+      System.out.println("10000");
+    } else if (libraryDueMonth - userReturnMonth < 0) {
+      int fine = (userReturnMonth - libraryDueMonth) * 500;
+      System.out.println(fine);
+    } else if (libraryDueDay - userReturnDay < 0) {
+      int fine = (userReturnDay - libraryDueDay) * 15;
+      System.out.println(fine);
+    } else {
+      System.out.println("0");
+    }    
+  }
+}
 
 //////////////////////// Day 27: Testing Part II!
 
